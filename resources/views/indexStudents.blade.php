dbb032dfaf24ec99d0f5cab25f562d6b92f717ea
Nino Chimento <saninoc84@gmail.com>
2020-03-17 14:31:12 +0100

    imposto lay

--- resources/views/indexStudents.blade.php
+++ resources/views/indexStudents.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.layout')
+@section('main')
 <div class="wrap-students">
-    @dd($students);
+    @foreach ($students as $student)
+        <div class="studente">
+        <h2>{{$student["name"]}}</h2>
+    </div>
+    @endforeach
+    
 </div>
+@endsection
