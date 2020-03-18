34f7a1b441ed817a23bd44437dd4b343b75fa179
Nino Chimento <saninoc84@gmail.com>
2020-03-18 13:42:36 +0100

    riesco a inviare e leggere il valore dell api....

--- app/Http/Controllers/Api/studentController.php
+++ app/Http/Controllers/Api/studentController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class studentController extends Controller
+class StudentController extends Controller
 {
     public function age(Request $request){
         $data = $request->input('age');
-        
+
+        $nino = ["nino"];
+        return response()->json($data);
+       
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::namespace("Api")->group(function() {
-    Route::post("/students/age","studentController@age");
+    Route::post("/students/age","StudentController@age");
 });
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
